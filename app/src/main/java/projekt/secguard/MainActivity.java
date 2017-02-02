4401382e41c9ccb65bad784ae63da03428adc8d3
DAFojt <blackdef.crysis@gmail.com>
2017-02-02 21:31:11 +0100

    test123

--- app/src/main/java/projekt/secguard/MainActivity.java
+++ app/src/main/java/projekt/secguard/MainActivity.java
@@ -21,6 +21,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intentLogin = new Intent(this, LoginActivity.class);
         startActivity(intentLogin);
         finish();
-
     }
 }
