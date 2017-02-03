38fa2007c756a95ae90701bef87703472d3b8691
KrzysztofBglk <rimen94@gmail.com>
2017-02-03 19:08:39 +0100

    Dodano testowe boczne menu

---
+++ app/src/main/java/projekt/secguard/UserTrackerActivity.java
@@ -0,0 +1,13 @@
+package projekt.secguard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserTrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_tracker);
+    }
+}
