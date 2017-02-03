62afcb955f2e9ef68ab5f7f4c4b22b10fa661c0b
KrzysztofBglk <rimen94@gmail.com>
2017-02-03 21:44:42 +0100

    UI access check

--- app/src/main/java/projekt/secguard/UserScreenActivity.java
+++ app/src/main/java/projekt/secguard/UserScreenActivity.java
@@ -1,10 +1,12 @@
 package projekt.secguard;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -15,7 +17,10 @@ public class UserScreenActivity extends AppCompatActivity {
     private ActionBarDrawerToggle mToggle;
     private TextView userLogin;
     private TextView userName;
+    private TextView test;
+    private UserData userData;
 
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +28,13 @@ public class UserScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_screen);
         userLogin = (TextView) findViewById(R.id.textLogin);
         userName = (TextView) findViewById(R.id.textNames);
-
+        test = (TextView) findViewById(R.id.testowe);
         // Uzyskiwanie danych o logowaniu z klasy UserData
         Intent intent = getIntent();
-        final UserData userData = (UserData) intent.getExtras().getSerializable("userData");
-        userLogin.setText("@"+userData.getLogin());
+        userData = (UserData) intent.getExtras().getSerializable("userData");
+        userLogin.setText(userData.getLogin());
         userName.setText(userData.getImie()+" "+userData.getNazwisko());
-
+        test.setText(userData.getPozycja());
 
         // Boczne menu
         mDrawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
@@ -37,6 +42,8 @@ public class UserScreenActivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);// back button
+        setMenuOptions();
+
     }
 
     // Podpięcie akcji rozwijania i zwijania menu z poziomu ikony górnego menu
@@ -46,5 +53,22 @@ public class UserScreenActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    // Dostosowanie menu pod uprawnienia usera
+    private void setMenuOptions()
+    {
+        navigationView = (NavigationView) findViewById(R.id.navigation);
+        Menu nav_Menu = navigationView.getMenu();
+        if(userData.getPozycja().equals("menager")){
+            nav_Menu.findItem(R.id.menu_myOrders).setVisible(false);
+            nav_Menu.findItem(R.id.menu_tracking).setVisible(false);
+        }
+        if(userData.getPozycja().equals("ochroniarz")){
+            nav_Menu.findItem(R.id.menu_editUsers).setVisible(false);
+            nav_Menu.findItem(R.id.menu_menageObjects).setVisible(false);
+            nav_Menu.findItem(R.id.menu_orders).setVisible(false);
+            nav_Menu.findItem(R.id.menu_finishedOrders).setVisible(false);
+        }
     }
 }
